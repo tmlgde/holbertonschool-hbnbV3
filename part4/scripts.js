8e18b75214598c06f99578de3c56209d85cdaa46
tmlgde <tom.lagarde33@outlook.fr>
2025-07-22 17:11:56 +0200

    first step for task1

--- part4/scripts.js
+++ part4/scripts.js
@@ -4,5 +4,46 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+      loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        console.log('Email:', email);
+        console.log('Password:', password);
+
+        fetch('http://localhost:5000/api/v1/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+            },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            })
+          })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Login failed');
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log('Login success', data);
+
+            //stock on cookie
+            document.cookie = "token=${data.acces_token}; path=/";
+            
+            //user going to index.html
+            window.location.href = 'index.html';
+          })
+          .catch(error => {
+            console.error('Error during login:', error);
+            alert('Login failed: ' + error.message);
+          });
+        });
+      }
+    });
